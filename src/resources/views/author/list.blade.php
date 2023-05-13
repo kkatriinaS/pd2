6d1cf61086bea89d7cc215338c2e8c19633685ed
unknown <katrina.sleze@gmail.com>
2023-05-13 16:00:24 +0300

    Iesākta autoru sadaļa

---
+++ src/resources/views/author/list.blade.php
@@ -0,0 +1,11 @@
+@extends('layout')
+
+@section('content')
+
+@if(count($items)>0)
+@else
+<p> Nav atrasts neviens ieraksts</p>
+@endif
+
+
+@endsection
---
+++ src/resources/views/home/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layout')
+
+@section('content')
+<h1>PD2 - {{ $title }} </h1>
+
+@endsection
